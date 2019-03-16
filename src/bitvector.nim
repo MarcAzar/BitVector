5d1f56f12111e74743bf9b0af1e45ff45f8773b7
Marc Azar <marc.azar@gmail.com>
2019-03-16 11:10:59 +0200

    Add Documentation
    Signed-off-by: Marc Azar <marc.azar@gmail.com>

--- src/bitvector.nim
+++ src/bitvector.nim
@@ -8,6 +8,39 @@
 ## BitVector is an in-memory bit vector, no mmap option is available at 
 ## the moment.
 ##
+## Usage
+## --------------
+##  ::
+##    import bitvector
+##  
+##    block:
+##      var ba = newBitVector[uint](64) # Create a BitVector with uint64 base
+##      echo ba # Prints capacity in bits = 64, and number of elements = 1
+##      ba[0] = 1 # Assign `true` to bit index `0`
+##      ba[2] = 1
+##      ba[7] = 1 # ba[0..7] now is `10000101` == 133
+##      assert(ba[0..7] == 133, "incorrect result: " & $ba[0..7]) 
+##      assert(ba[0..4] == 5, "incorrect result: " & $ba[0..4])
+##      assert(ba[1..4] == 2, "incorrect result: " & $ba[1..4])
+##  
+##    var bitarrayA = newBitVector[uint](2e9)
+##    bitarrayA[0] = 1
+##    bitarrayA[1] = 1
+##    bitarrayA[2] = 1
+##
+##    # Test range lookups/inserts
+##    bitarrayA[65] = 1
+##    doAssert bitarrayA[65] == 1
+##    bitarrayA[131] = 1
+##    bitarrayA[194] = 1
+##    assert bitarrayA[2..66] == bitarrayA[131..194]
+##
+##    let sliceValue = bitarrayA[131..194]
+##    bitarrayA[270..333] = sliceValue
+##    bitarrayA[400..463] = uint(-9223372036854775807)
+##    assert bitarrayA[131..194] == bitarrayA[270..333]
+##    assert bitarrayA[131..194] == bitarrayA[400..463]
+##
 type 
   Bit = range[0..1]
   BitVector*[T: SomeUnsignedInt] = object
